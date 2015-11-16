9f38e42e915746dc8e70546273e8ba6df5a91dff
AnTruong <little.fairy1791@gmail.com>
2015-11-17 06:53:06 +0700

    project henbacsi frontend

---
+++ assets/scripts/main.js
@@ -0,0 +1,27 @@
+﻿ function registerCompanyItemClick(){
+        $(".companies-item-container .logo-container").click(function() {
+          var displaytarget = $(this).attr("data-target");
+          $(".company-description").hide();
+          $("#"+displaytarget).slideDown();
+        });
+    }
+function registerMenuScroll(){
+	$(".menu-item a").click(function() {
+	  var displaytarget = $(this).attr("data-target");
+	  scrollToElement(displaytarget);
+	});
+}
+function scrollToLinkView(){
+	var pathname = window.location.href;
+	var splittedPath = pathname.split("#");
+	if(splittedPath.length>1){
+		var idToScroll = splittedPath[splittedPath.length-1];
+		scrollToElement(idToScroll);
+	}
+}
+function scrollToElement(elementId){
+	targetElement =  $("#"+elementId);
+	var scrollTo =  targetElement.offset().top -120;
+	$('html, body').animate({
+	scrollTop: scrollTo}, 1000);
+}
